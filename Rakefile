23abe6281398187391879c7a9323c55faac4d164
filepang <filepang@lycos.co.kr>
2013-07-15 19:00:35 +0900

    add a rakefile

---
+++ Rakefile
@@ -0,0 +1,8 @@
+require "rake/testtask"
+
+Rake::TestTask.new do |t|
+  t.verbose = true
+end
+
+desc "Run tests"
+task :default => :test
--- test/test_proc_bind.rb
+++ test/test_proc_bind.rb
@@ -1,44 +1,31 @@
-$: << "../lib"
+require "test/unit"
 require "proc_bind"
 
-describe 'Proc with 1 argument' do
-    let(:p1) { Proc.new { |a| a } }
-    let(:p0) { p1.bind(1, "hello") }
+class ProcBindTest <  Test::Unit::TestCase
+  def test_proc_with_1_arg
+    p1 =  Proc.new { |a| a }
+    p0 = p1.bind(1, "hello")
 
-    it "recevie no argument" do
-        p0.parameters.should be_empty
-    end
+    assert_equal p0.parameters.size, 0
+    assert_equal p0.call, "hello"
+  end
 
-    it "pass correct parameter" do
-        p0.call.should == "hello"
-    end
-end
+  def test_proc_with_2_arg
+    p2 = Proc.new { |a, b| [a, b].join(" ") }
+    p1 = p2.bind(2, "world")
 
-describe 'Proc with 2 argument' do
-    let(:p2) { Proc.new { |a, b| [a, b].join(" ") } }
-    let(:p1) { p2.bind(2, "world") }
+    assert_equal p1.parameters.size, 1
+    assert_equal p1.call("hello"), "hello world"
+  end
 
-    it "recevie 1 argument" do
-        p1.parameters.size.should == 1
-    end
+  def test_proc_with_4_arg
+    p4 = Proc.new { |a, b, c, d| [a, b, c, d].join(" ") }
+    p3 = p4.bind(4, "ruby")
+    p2 = p3.bind(2, "world")
+    p1 = p2.bind(2, "from")
+    p0 = p1.bind(1, "hello")
 
-    it "pass correct parameter" do
-        p1.call("hello").should == "hello world"
-    end
-end
-
-describe 'Proc with 4 argument' do
-    let(:p4) { Proc.new { |a, b, c, d| [a, b, c, d].join(" ") } }
-    let(:p3) { p4.bind(4, "ruby") }
-    let(:p2) { p3.bind(2, "world") }
-    let(:p1) { p2.bind(2, "from") }
-    let(:p0) { p1.bind(1, "hello") }
-
-    it "recevie no argument" do
-        p0.parameters.should be_empty
-    end
-
-    it "pass correct parameter" do
-        p0.call.should == "hello world from ruby"
-    end
+    assert_equal p0.parameters.size, 0
+    assert_equal p0.call,"hello world from ruby"
+  end
 end
